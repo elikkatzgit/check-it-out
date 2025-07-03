08a01bf1b32615f769db9483e02473adf45ba4fd
elikkatzgit <50174655+elikkatzgit@users.noreply.github.com>
2025-07-03 23:07:32 +0300

    Update ec2.tf

--- code/ec2.tf
+++ code/ec2.tf
@@ -33,7 +33,7 @@ resource "aws_route_table_association" "public_assoc" {
 resource "aws_instance" "microservice_1" {
   ami           = "ami-0c02fb55956c7d316"  
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.microservice1_sg.name]
+  security_groups = [aws_security_group.microservice_sg.name]
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
   root_block_device {
     volume_size = 16      
