d7291df2aa7bfacd6825514c0ab4a350791ffaf2
elikkatzgit <50174655+elikkatzgit@users.noreply.github.com>
2025-07-03 22:20:02 +0300

    Update ec2.tf

--- code/ec2.tf
+++ code/ec2.tf
@@ -5,6 +5,25 @@ resource "aws_subnet" "private_subnet" {
   subnet_id = aws_subnet.private_subnet.id
   map_public_ip_on_launch = false
 }
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
 
 # microservice1 instance
 resource "aws_instance" "microservice_1" {
