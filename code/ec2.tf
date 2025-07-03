70f7e108e4a01e38d927eb89048c6dddf50e7a85
elikkatzgit <50174655+elikkatzgit@users.noreply.github.com>
2025-07-03 22:35:22 +0300

    Update ec2.tf

--- code/ec2.tf
+++ code/ec2.tf
@@ -1,18 +1,18 @@
 resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.check_vpc.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   subnet_id = aws_subnet.private_subnet.id
   map_public_ip_on_launch = false
 }
 resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.check_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.check_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
